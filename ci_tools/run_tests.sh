fe73ce63f51a32f9637dd1f438f0474853906b0d
Sylvain MARIE <sylvain.marie@se.com>
2019-07-04 14:30:45 +0200

    CI runs will now take less time because coverage is only run on one job (the deploy one)

--- ci_tools/run_tests.sh
+++ ci_tools/run_tests.sh
@@ -12,12 +12,15 @@ cleanup() {
 
 trap "cleanup" INT TERM EXIT
 
-#if hash pytest 2>/dev/null; then
-#    echo "pytest found"
-#else
-#    echo "pytest not found. Trying py.test"
-#fi
-
-# First the raw for coverage
-echo -e "\n\n****** Running tests ******\n\n"
-python -m pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html --cov-report term-missing --cov=./getversion -v getversion/tests/
+if [ "${TRAVIS_PYTHON_VERSION}" = "3.5" ]; then
+   # full
+   # Run tests with "python -m pytest" to use the correct version of pytest
+   echo -e "\n\n****** Running tests ******\n\n"
+   coverage run --source getversion -m pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html -v getversion/tests/
+   # buggy
+   # python -m pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html --cov-report term-missing --cov=./getversion -v getversion/tests/
+else
+   # faster - skip coverage and html report but keep junit (because used in validity threshold)
+    echo -e "\n\n****** Running tests******\n\n"
+    python -m pytest --junitxml=reports/junit/junit.xml -v getversion/tests/
+fi
