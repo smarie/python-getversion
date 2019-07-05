bb395849888b513965dabc8a6abb27af3b4d25ba
Sylvain MARIE <sylvain.marie@se.com>
2019-07-05 19:36:37 +0200

    Trying to debug what happens in travis

--- ci_tools/write_version.py
+++ ci_tools/write_version.py
@@ -2,15 +2,17 @@
 #
 #  License: BSD 3 clause
 
+from os.path import abspath
+
 import click
 from setuptools_scm import get_version
 
 
 @click.command()
-@click.argument('pkg_name')
-def write_version(pkg_name):
-    file_name = '%s/_version.py' % pkg_name
-    print("Writing version to file: %s" % file_name)
+@click.argument('dest_folder')
+def write_version(dest_folder):
+    file_name = '%s/_version.py' % dest_folder
+    print("Writing version to file: %s" % abspath(file_name))
     get_version('.', write_to=file_name)
 
 
